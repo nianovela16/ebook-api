ec31ac2ea6c8bdf18e792bcbdfb6e9714beefe1a
nianovela16 <novelaariandini@gmail.com>
2022-11-02 15:13:21 -0700

    auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -12,6 +14,66 @@ class AuthController extends Controller
             'Nama' => 'Nia Novela Ariandini',
             'Phone' => '081392150129',
             'Class' => 'XII RPL 5',
+        ];
+    }
+
+    public function register(Request $request){
+
+        $field = $request->validate([
+            'name' => 'required|string|max:100',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed|min:8'
+        ]);
+
+        $user = User::create([
+            'name' => $field['name'],
+            'email' => $field['email'],
+            'password' => bcrypt($field['password'])
+        ]);
+
+        $token = $user->CreateToken('tokenku')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        //check email
+        $user = User::where('email', $fields['email'])->first();
+
+        //check password
+        if (!$user || !Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message' => 'unauthorized'
+            ], 401);
+        }
+
+        $token = $user->createToken('tokenku')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+    public function logout (Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return[
+            'message' => 'logout'
         ];
     }
 }
